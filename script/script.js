681a04b7b374f8412f9b74b011ccab94ffc9999d
Camille Mallet <mallet.camille11@gmail.com>
2023-08-30 09:11:58 +0200

    refacto(Js)

--- script/script.js
+++ script/script.js
@@ -17,51 +17,61 @@ const slides = [
 	}
 ]
 
-	document.body.onload=function () {
+	document.body.onload = function () {
 		let p=0 // position à 0 sur la 1ère image //
-		container=document.getElementById("banner"); // on définit le container du carrousel //
-		const arrow_left=document.getElementById("arrow_left"); // on définit l'élément flèche gauche //
-		const arrow_right=document.getElementById("arrow_right"); // on définit l'élément flèche droite //
 
+		// Récupération des éléments de la page //
+		container = document.getElementById("banner"); // on définit le container du carrousel //
+		const arrow_left = document.getElementById("arrow_left"); // on définit l'élément flèche gauche //
+		const arrow_right = document.getElementById("arrow_right"); // on définit l'élément flèche droite //
+
+		// Ajout des Event listeners //
+		arrow_left.addEventListener("click", clickFlecheGauche);
+		arrow_right.addEventListener("click", clickFlecheDroite);
+
+		// Fonction qui update les images, les taglines et les bullet points //
 		function updateSlide() {
+			// Récupération des éléments de la page //
 			let imageSlide = document.getElementsByClassName("banner-img")[0];
-			imageSlide.setAttribute ("src","./assets/images/slideshow/" + slides[p].image);
 			let tagLine = document.querySelector("#banner p");
+			// Attribution des nouvelles valeurs //
+			imageSlide.setAttribute ("src","./assets/images/slideshow/" + slides[p].image);
 			tagLine.innerHTML=slides[p].tagLine;
+			// Update du Bullet Point //
+			// On retire la sélection actuelle //
 			let dotSelected = document.querySelector(".dot_selected");
 			dotSelected.classList.remove('dot_selected');
+			// On sélectionne la nouvelle position //
 			let updateDotSelected = document.querySelector(".dots :nth-child("+(p+1)+")");
 			updateDotSelected.classList.add("dot_selected");
 
 		}
 		
 		function clickFlecheGauche () {
-			p-=1;
-			if (p<0) {
-				p=nbrSlides-1;
+			p -= 1;
+			if (p < 0) {
+				p = nbrSlides - 1;
 			}	
 			updateSlide();
 		}
 		
 		function clickFlecheDroite () {
-			p+=1;
-			if (p>=nbrSlides) {
-				p=0;
+			p += 1;
+			if (p >= nbrSlides) {
+				p = 0;
 
 			}
 			updateSlide();
 		}	
 
-		arrow_left.addEventListener("click", clickFlecheGauche);
-		arrow_right.addEventListener("click", clickFlecheDroite);
-
+		// Ajout des bullet points //
 		let elementDots = document.getElementsByClassName("dots")[0]; // On vient sélectionner le 1er élément à l'index 0 //
 		let nbrSlides = slides.length // On compte le nombre d'éléments dans le tableau slides //
 		for (let i=0; i<nbrSlides; i++) { 
 			let nouvelleDiv = document.createElement("div");
 			elementDots.appendChild(nouvelleDiv);
 			nouvelleDiv.classList.add('dot');
-			if (i===p) { // dans le cas où il s'agit de la 1ère div, c'est celle-ci à laquelle on va vouloir attribuer la classe dot_selected //
+			if (i===p) { // si l'index est égal à p alors c'est la 1ère slide qu'on sélectionne par défaut donc on ajoute la classe css dot_selected //
 				nouvelleDiv.classList.add('dot_selected'); // sinon rien donc pas besoin d'indiquer un else //
 			} 		
 		} 
